86b01979999cccc1c589bcd5bb1a01dd8521b5f2
Zhubo Zhou <zhubozho@gmail.com>
2023-04-16 15:33:47 -0700

    minor

--- serverA.cpp
+++ serverA.cpp
@@ -108,10 +108,11 @@ vector<int> find_intersection(vector<string> names, schedules &scheds)
             int x_end = intersects[j + 1];
             int y_start = sched[i].first;
             int y_end = sched[i].second;
-            cout << "x_start=" << x_start << " x_end=" << x_end << " y_start=" << y_start << " y_end=" << y_end << endl;
 
-            if (x_start < y_end || y_start < x_end) 
+            // check if there is an intersection
+            if ((x_start <= y_end && y_start <= x_end) || (y_start <= x_end && x_start <= y_end))
             {
+                cout << "x_start=" << x_start << " x_end=" << x_end << " y_start=" << y_start << " y_end=" << y_end << endl;
                 new_intersects.push_back(max(x_start, y_start));
                 new_intersects.push_back(min(x_end, y_end));
             }
