1e45951867153a81fc494ac555ad1dac4d7cf75e
Zhubo Zhou <zhubozho@gmail.com>
2023-04-20 04:04:41 -0700

    minor

--- serverB.cpp
+++ serverB.cpp
@@ -267,6 +267,10 @@ int main(int argc, const char* argv[])
 
     // find the time intersection among them
     vector<int> intersects = find_intersection(names, scheds);
+    // handle the case when one of the user's sched is empty
+    if (intersects.size() == 2 && intersects[0] == 0 && intersects[1] == 0) {
+        intersects.clear();
+    }
     char intersects_buf[INTERSECTS_BUF_SIZE];
     vec_to_buf(intersects, intersects_buf);
 
