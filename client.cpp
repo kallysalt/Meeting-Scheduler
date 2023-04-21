e0c064b260eb0d1ddfa36205ae3cebb6ec130030
Zhubo Zhou <zhubozho@gmail.com>
2023-04-20 20:51:48 -0700

    minor

--- client.cpp
+++ client.cpp
@@ -99,6 +99,7 @@ int main(int argc, const char* argv[]){
         // show a prompt: Please enter the usernames to check schedule availability:
         cout << "Please enter the usernames to check schedule availability:" << endl;
         getline(cin, input);
+        cin.ignore();
 
         // send these names to the main server over tcp (from beej's guide)
         if (send(sockfd, input.c_str(), input.length(), 0) == -1)
