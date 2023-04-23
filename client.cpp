6ae23d924c327f163844d8c5ccf6a968316d16c4
Zhubo Zhou <zhubozho@gmail.com>
2023-04-23 00:07:24 -0700

    minor

--- client.cpp
+++ client.cpp
@@ -177,40 +177,50 @@ int main(int argc, const char* argv[]){
 
         // print valid names
         // parse input, store in a vector
-        char input_buf [USERNAMES_BUF_SIZE];
-        strcpy(input_buf, input.c_str());
-        vector<string> input_names = buf_to_string_vec(input_buf);
-        // parse invalid puf, store in a set, if name is not in the invalid set, print it
-        cout << " works for ";
-        if (strcmp(invalid_buf, "pass") == 0) 
+        // char input_buf [USERNAMES_BUF_SIZE];
+        // strcpy(input_buf, input.c_str());
+        // vector<string> input_names = buf_to_string_vec(input_buf);
+        // // parse invalid puf, store in a set, if name is not in the invalid set, print it
+        // cout << " works for ";
+        // if (strcmp(invalid_buf, "pass") == 0) 
+        // {
+        //     for (int i = 0; i < input_names.size(); i++) 
+        //     {
+        //         cout << input_names[i];
+        //         // print "," if not the last element
+        //         if (i != input_names.size() - 1) {
+        //             cout << ", ";
+        //         }
+        //     }
+        // }
+        // else {
+        //     set<string> invalid_names;
+        //     invalid_names = buf_to_string_set(invalid_buf);
+        //     int num_valid_names = input_names.size() - invalid_names.size();
+        //     for (int i = 0; i < input_names.size(); i++) 
+        //     {
+        //         if (invalid_names.find(input_names[i]) == invalid_names.end()) 
+        //         {
+        //             cout << input_names[i];
+        //             // print "," if not the last valid name
+        //             if (num_valid_names > 1) {
+        //                 cout << ", ";
+        //                 num_valid_names--;
+        //             }
+        //         }
+        //     }
+        // }
+        // cout << "." << endl;
+
+        // receive valid user names from server m over tcp (from beej's guide)
+        char valid_buf[CLIENT_MAXDATASIZE];
+        memset(valid_buf, 0, sizeof(valid_buf));
+        if ((numbytes = recv(sockfd, valid_buf, CLIENT_MAXDATASIZE - 1, 0)) == -1)
         {
-            for (int i = 0; i < input_names.size(); i++) 
-            {
-                cout << input_names[i];
-                // print "," if not the last element
-                if (i != input_names.size() - 1) {
-                    cout << ", ";
-                }
-            }
+            perror("client: recv");
+            exit(1);
         }
-        else {
-            set<string> invalid_names;
-            invalid_names = buf_to_string_set(invalid_buf);
-            int num_valid_names = input_names.size() - invalid_names.size();
-            for (int i = 0; i < input_names.size(); i++) 
-            {
-                if (invalid_names.find(input_names[i]) == invalid_names.end()) 
-                {
-                    cout << input_names[i];
-                    // print "," if not the last valid name
-                    if (num_valid_names > 1) {
-                        cout << ", ";
-                        num_valid_names--;
-                    }
-                }
-            }
-        }
-        cout << "." << endl;
+        cout << " works for " << valid_buf << "." << endl;
 
         // start a new request 
         input.clear();
